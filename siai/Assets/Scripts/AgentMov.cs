78053313f14358a8bb699ded9a5314b40eae5413
Tranfigured-Requiem <tranfiguredrequiem@gmail.com>
2021-08-13 14:33:55 +0100

    Creaing siai
    nothing notable

---
+++ siai/Assets/Scripts/AgentMov.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentMov : MonoBehaviour
+{
+    [SerilizeField]
+    private int speed = 100;
+    
+}
